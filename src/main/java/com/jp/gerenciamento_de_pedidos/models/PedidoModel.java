24215224dfe9ac9fe990a438654a1909c4162e31
João Paulo <joaopaulotraguetta14@gmail.com>
2026-03-21 16:11:17 -0300

    pedidomodel

--- src/main/java/com/jp/gerenciamento_de_pedidos/models/PedidoModel.java
+++ src/main/java/com/jp/gerenciamento_de_pedidos/models/PedidoModel.java
@@ -1,4 +1,56 @@
 package com.jp.gerenciamento_de_pedidos.models;
 
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_pedidos")
 public class PedidoModel {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "dataPedido")
+    private LocalDateTime data;
+    @Column(name= "valorTotalPedido")
+    private BigDecimal valorTotal;
+    private String status;
+
+    public PedidoModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
--- src/main/java/com/jp/gerenciamento_de_pedidos/services/PedidoService.java
+++ src/main/java/com/jp/gerenciamento_de_pedidos/services/PedidoService.java
@@ -1,4 +1,7 @@
 package com.jp.gerenciamento_de_pedidos.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PedidoService {
 }
